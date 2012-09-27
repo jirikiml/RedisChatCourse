841c36924d20d54a828724cefbb84ac3621f12e7
Jiri Kiml <jiri.kiml@post.cz>
2012-09-27 21:47:00 +0200

    few more warings added for testing purposes

--- src/test/java/cz/kiml/WarningsHelper.java
+++ src/test/java/cz/kiml/WarningsHelper.java
@@ -1,20 +1,28 @@
 package cz.kiml;
 
+import java.util.HashMap;
+
 /**
- * Class with many problems to be able to test behavior of eclipse/hudson plugins for findbugs, task scanner, ...
+ * Class with many problems to be able to test behavior of eclipse/hudson
+ * plugins for findbugs, task scanner, ...
+ * 
  * @author kiml
- *
  */
 public class WarningsHelper
 {
     // FIXME some fixme
     // TODO some todo
     private int x;
-    
-    public void testMethod(int q) {
-        if (true)
-            return;
+
+    public void testMethod(int q)
+    {
         int x = 12;
+        String person = new HashMap<String, String>().get("A");
+        if (person != null)
+        {
+            person.toCharArray();
+        }
+        String name = person.toString();
     }
 
 }
